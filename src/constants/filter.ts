ec5eedba0970523e33b1b90e9e57cf67d3b4e19d
vidit <viditsharma818@gmail.com>
2024-05-04 15:40:00 +0530

    filters constant done

---
+++ src/constants/filter.ts
@@ -0,0 +1,43 @@
+export const MinExperience = [
+  { value: 0, label: "0 Years" },
+  { value: 1, label: "1 Year" },
+  { value: 2, label: "2 Years" },
+  { value: 3, label: "3 Years" },
+  { value: 4, label: "4 Years" },
+  { value: 5, label: "5 Years" },
+  { value: 6, label: "6 Years" },
+  { value: 7, label: "7 Years" },
+  { value: 8, label: "8 Years" },
+  { value: 9, label: "9 Years" },
+  { value: 10, label: "10 Years" },
+  { value: 11, label: "11 Years" },
+  { value: 12, label: "12 Years" },
+  { value: 13, label: "13 Years" },
+  { value: 14, label: "14 Years" },
+  { value: 15, label: "15 Years" },
+];
+
+export const Location = [
+  { value: "delhi ncr", label: "Delhi NCR" },
+  { value: "mumbai", label: "Mumbai" },
+  { value: "bangalore", label: "Bangalore" },
+  { value: "hyderabad", label: "Hyderabad" },
+  { value: "chennai", label: "Chennai" },
+  { value: "pune", label: "Pune" },
+  { value: "kolkata", label: "Kolkata" },
+  { value: "ahmedabad", label: "Ahmedabad" },
+  { value: "chandigarh", label: "Chandigarh" },
+];
+
+export const Role = [
+  { value: "frontend", label: "Frontend Developer" },
+  { value: "backend", label: "Backend Developer" },
+  { value: "fullstack", label: "Fullstack Developer" },
+  { value: "ios", label: "IOS Developer" },
+  { value: "android", label: "Android Developer" },
+  { value: "qa", label: "QA Engineer" },
+  { value: "devops", label: "DevOps Engineer" },
+  { value: "product", label: "Product Manager" },
+  { value: "tech lead", label: "Tech Lead" },
+  { value: "engineering manager", label: "Engineering Manager" },
+];
